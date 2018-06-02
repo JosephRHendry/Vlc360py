99a198f46f62551dac798394f93fd6e3d0b6903e
Joseph Hendry <38083167+JosephRHendry@users.noreply.github.com>
2018-06-02 14:51:24 -0700

    Update ob_threading.py

--- ob_threading.py
+++ ob_threading.py
@@ -1,10 +1,15 @@
-# Work in Progress to rewrite class threads
-
 import threading
 
 class ob_Thread(threading.Thread):
     def __init__(self):
         self.active = True
+        self.stop_event = threading.Event()
+
+    def stop(self):
+        self._stop_event.set()
+
+    def stopped(self):
+        return self._stop_event.is_set()
 
     def run(self):
         """Method representing the thread's activity.
@@ -13,11 +18,14 @@ class ob_Thread(threading.Thread):
         target argument, if any, with sequential and keyword arguments taken
         from the args and kwargs arguments, respectively.
         """
-        try:
-            if self._target:
-                self._target(*self._args, **self._kwargs)
-        finally:
-        # Avoid a recycle if the thread is running a function with
-        # an argument that has a member that points to the thread.
 
-            del self._target, self._args, self._kwargs
+        while not self.stop_event.is_set():
+            try:
+                if self._target:
+                    self._target(*self._args, **self._kwargs)
+            finally:
+            # Avoid a recycle if the thread is running a function with
+            # an argument that has a member that points to the thread.
+
+                del self._target, self._args, self._kwargs
+
